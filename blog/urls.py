173e039d0ce44872523db0d5b74c8aa155fe6bb6
KHS <123@123>
2024-05-26 19:55:36 +0900

    [feat] 기능 구현

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', PostListCreateAPIView.as_view(), name='post-list-create'),
+    path('<int:pk>',PostRetrieveUpdateDestroyAPIview.as_view(), name='post-detail'),
+]
