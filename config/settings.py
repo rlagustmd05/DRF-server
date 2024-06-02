43a408f454f1960403e9f1f7179c84174be82680
KHS <123@123>
2024-06-02 22:38:49 +0900

    modify finalllll????

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,7 @@ load_dotenv()
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = 'True'
 
 
 # Application definition
