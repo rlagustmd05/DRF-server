8a0b9050732c189208ff6212675b61bfbc430f88
KHS <123@123>
2024-06-02 22:11:51 +0900

    modify final2

--- config/settings.py
+++ config/settings.py
@@ -176,11 +176,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
-ALLOWED_HOSTS = [
-    'http://127.0.0.1:8000',
-    '127.0.0.1',
-    'port-0-drf-server-ss7z32llwun1pv1.sel5.cloudtype.app',
-]
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
